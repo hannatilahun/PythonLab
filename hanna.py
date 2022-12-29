13189da5290d85c5782f9c1a103ea917700210c3
hanna <hannand@gmail.com>
2022-12-29 13:18:47 +0000

    add two num

--- hanna.py
+++ hanna.py
@@ -1,14 +1,15 @@
 def sum():
     print("hello every one this is our firest python lab class")
-    a,b,c=5,7,8
-    d=a+b+c
-    print(c)
-    x,y,z="orang","banna","chery"
-    print(x)
-    print(y)
-    print(z)
-    x=y=z="orang"
-    print(x)
-    print(y)
-    print(z)
+    
+    a="hanna"
+    b=23
+    c=453.645
+    print ("my name is {1} and i am  {0} years old ".format(a,b,c))
+    print(f"")
+
+
 sum()
+
+
+
+
