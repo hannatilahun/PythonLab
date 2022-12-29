bf71886e87d5042334afb7460da2c236fb6c62df
hanna <hannand@gmail.com>
2022-12-29 12:28:23 +0000

    Merge branch 'main' of https://github.com/hannatilahun/PythonLab

--- Untitled-1.py
+++
@@ -1,1 +0,0 @@
-print("suidy87sfs")
---
+++ hanna.py
@@ -0,0 +1,14 @@
+def sum():
+    print("hello every one this is our firest python lab class")
+    a,b,c=5,7,8
+    d=a+b+c
+    print(c)
+    x,y,z="orang","banna","chery"
+    print(x)
+    print(y)
+    print(z)
+    x=y=z="orang"
+    print(x)
+    print(y)
+    print(z)
+sum()
