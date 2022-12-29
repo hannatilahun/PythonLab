e3ecb3dac16481ed4f4b8af108db10927dbe68cd
hanna <hannand@gmail.com>
2022-12-29 13:29:05 +0000

    add two num

--- hanna.py
+++ hanna.py
@@ -1,6 +1,18 @@
 def sum():
     print("hello every one this is our firest python lab class")
     
+    a,b,c=5,7,8
+    d=a+b+c
+    print(c)
+    x,y,z="orang","banna","chery"
+    print(x)
+    print(y)
+    print(z)
+    x=y=z="orang"
+    print(x)
+    print(y)
+    print(z)
+
     a="hanna"
     b=23
     c=453.645
