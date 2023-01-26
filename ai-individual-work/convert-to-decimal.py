59f9e56407eb1c7d8da326b990f68943cd4e440a
ty-ab <111655133+ty-ab@users.noreply.github.com>
2023-01-26 23:40:25 +0300

    modified it to not accept four consecutive roman number

--- ai-individual-work/convert-to-decimal.py
+++ ai-individual-work/convert-to-decimal.py
@@ -1,36 +1,50 @@
 """
 name : Taye Birhanu
 id : DBUR/0963/12
-"""
 
+AI individual Assignment
+"""
 
 def roman_to_decimal(roman_numeral):
     decimal_value = 0
     roman_to_decimal = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     prev_value = 0
+    char_freq = []
+    K = 4
+    current_char, count = None, 0
 
     try:
 
         for i in roman_numeral[::-1]:
+            if i == current_char:
+                count += 1
+            else:
+                current_char,count = i,1
+
+            if count == K:
+                char_freq.append(K * i)
+                return print("Invalid Roman Number; No 4 Consecutive Roman Number ->", char_freq[0])
+
             if roman_to_decimal[i] < prev_value:
                 decimal_value -= roman_to_decimal[i]
             else:
                 decimal_value += roman_to_decimal[i]
             prev_value = roman_to_decimal[i]
-        return decimal_value
 
     except:
-        return "Invalid Input -> ",roman_numeral
+        return print("Invalid input ->",roman_numeral)
+
+    return print("The Roman Number is: ","[",decimal_value,"]")
+
 
 
 while True:
 
     roman_number = input("Enter Roman number: ")
 
-    print("The Roman number is: ","[",roman_to_decimal(roman_number.upper()),"]")
+    roman_to_decimal(roman_number.upper())
 
     choice = input("Convert again?, \'Y\' for yes/anykey for no: ")
 
     if choice.lower() != "y":
         break
-
