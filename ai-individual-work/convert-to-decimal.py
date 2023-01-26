87a5c12e71c7996c93e5b0ab150519a82265158c
ty-ab <111655133+ty-ab@users.noreply.github.com>
2023-01-26 19:37:38 +0300

    added try and catch for invalid input

--- ai-individual-work/convert-to-decimal.py
+++ ai-individual-work/convert-to-decimal.py
@@ -3,17 +3,24 @@ name : Taye Birhanu
 id : DBUR/0963/12
 """
 
+
 def roman_to_decimal(roman_numeral):
     decimal_value = 0
     roman_to_decimal = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     prev_value = 0
-    for i in roman_numeral[::-1]:
-        if roman_to_decimal[i] < prev_value:
-            decimal_value -= roman_to_decimal[i]
-        else:
-            decimal_value += roman_to_decimal[i]
-        prev_value = roman_to_decimal[i]
-    return decimal_value
+
+    try:
+
+        for i in roman_numeral[::-1]:
+            if roman_to_decimal[i] < prev_value:
+                decimal_value -= roman_to_decimal[i]
+            else:
+                decimal_value += roman_to_decimal[i]
+            prev_value = roman_to_decimal[i]
+        return decimal_value
+
+    except:
+        return "Invalid Input -> ",roman_numeral
 
 
 while True:
@@ -26,3 +33,4 @@ while True:
 
     if choice.lower() != "y":
         break
+
